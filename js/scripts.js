d23d517e1f27c4ae38240563be2c57a18c1e3217
Annie Sonna and Rafael Perez <annie@>
2016-10-18 16:17:27 -0700

    jquery for append, prepend methods and the remove item

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,34 @@
-$(function() {
-  $("span.clickable1").click(function() {
-    $(this).slideUp();
-    //shows the following span tag
-    $(this).next().slideDown();
-
-  });
-  $("span.clickable2").click(function() {
-    // $(this).hide();
-    //
-    // $(this).prev().show();
-    $(this).slideUp();
-    //shows the following span tag
-    $(this).prev().slideDown();
+$(document).ready(function() {
+  $("button#hello").click(function() {
+    $("ul#user").append("<li>Hello!</li>");
+    $("ul#webpage").append("<li>Hello there!</li>");
+    $("ul#user").children("li").click(function(){
+      // $(this).remove();
+    });
+    $("ul#webpage").children("li").click(function(){
+      $("li").remove();
+    });
   });
 
+  $("button#goodbye").click(function() {
+    $("ul#user").append("<li>Goodbye!</li>");
+    $("ul#webpage").append("<li>Thank you, Goodbye!</li>");
+    $("ul#user").children("li").click(function(){
+      // $(this).remove();
+    });
+    $("ul#webpage").children("li").click(function(){
+      // $(this).remove();
+    });
+  });
+
+  $("button#stop").click(function() {
+    $("ul#user").append("<li>Stop copying me!</li>");
+    $("ul#webpage").append("<li>No worry, I can find something better to do!</li>");
+    $("ul#user").children("li").click(function(){
+      // $(this).remove();
+    });
+    $("ul#webpage").children("li").click(function(){
+      // $(this).remove();
+    });
+  });
 });
